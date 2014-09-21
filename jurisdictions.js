aaf51927094b05776e509396c4c497da7a126ff0
Jackson Hull <jackson@jacksonhull.com>
2014-09-21 19:06:31 +0100

    updating config

--- jurisdictions.js
+++ jurisdictions.js
@@ -16,30 +16,11 @@ exports.regions = {
     "label": "United States",
     "jurisdictions": [
       {
-        "name": "us-ny",
-        "label": "New York",
-        "description": "NDA valid in New York.",
-        "limitations": "None noted",
-        "language": "en-us"
-      },
-      {
         "name": "us-ca",
         "label": "California",
         "description": "NDA valid in California.",
         "limitations": "None noted",
         "language": "en-us"
-      }
-    ]
-  },
-  "fr": {
-    "label": "France",
-    "jurisdictions": [
-      {
-        "name": "fr",
-        "label": "France",
-        "description": "NDA valid in France.",
-        "limitations": "None noted",
-        "language": "fr-fr"
       }
     ]
   }
@@ -86,66 +67,6 @@ exports.jurisdictions = {
   },
   "us-ca": {
     "company_fields": [
-      { "name": "address",
-        "label": "Address",
-        "type": "textarea"
-      },
-      { "name": "signatory_name",
-        "label": "Signatory name",
-        "type": "text"
-      },
-      { "name": "signatory_title",
-        "label": "Signatory title",
-        "type": "text"
-      }
-    ],
-    "recipient_fields": [
-      { "name": "company",
-        "label": "Company",
-        "type": "text",
-        "mandatory": false
-      },
-      { "name": "title",
-        "label": "Title",
-        "type": "text",
-        "mandatory": false
-      }
-    ]
-  },
-  "us-ny": {
-    "company_fields": [
-      { "name": "address",
-        "label": "Address",
-        "type": "textarea"
-      },
-      { "name": "signatory_name",
-        "label": "Signatory name",
-        "type": "text"
-      },
-      { "name": "signatory_title",
-        "label": "Signatory title",
-        "type": "text"
-      }
-    ],
-    "recipient_fields": [
-      { "name": "company",
-        "label": "Company",
-        "type": "text",
-        "mandatory": false
-      },
-      { "name": "title",
-        "label": "Title",
-        "type": "text",
-        "mandatory": false
-      }
-    ]
-  },
-  "fr": {
-    "company_fields": [
-      { "name": "registration_number",
-        "label": "Registration number",
-        "type": "text"
-      },
       { "name": "address",
         "label": "Address",
         "type": "textarea"
